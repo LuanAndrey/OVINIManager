a218e0178ee996533fc3ac1341ceab3cb3f3fc1c
Luan Andrey <luandrey.99@hotmail.com>
2023-11-07 22:02:37 -0300

    Initial commit

---
+++ OVINIManager/Inicializador.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace OVINIManager
+{
+    partial class Inicializador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblAbduzidos = new System.Windows.Forms.Label();
+            this.lblTripulantes = new System.Windows.Forms.Label();
+            this.lblPlanetaOrigem = new System.Windows.Forms.Label();
+            this.nudAbduzidos = new System.Windows.Forms.NumericUpDown();
+            this.nudTripulantes = new System.Windows.Forms.NumericUpDown();
+            this.cmbPlaneta = new System.Windows.Forms.ComboBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.pbNave = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAbduzidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTripulantes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNave)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(72, 6);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(228, 38);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "OVNI Manager ";
+            // 
+            // lblAbduzidos
+            // 
+            this.lblAbduzidos.AutoSize = true;
+            this.lblAbduzidos.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbduzidos.Location = new System.Drawing.Point(152, 80);
+            this.lblAbduzidos.Name = "lblAbduzidos";
+            this.lblAbduzidos.Size = new System.Drawing.Size(106, 18);
+            this.lblAbduzidos.TabIndex = 1;
+            this.lblAbduzidos.Text = "Max Abduzidos:";
+            // 
+            // lblTripulantes
+            // 
+            this.lblTripulantes.AutoSize = true;
+            this.lblTripulantes.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTripulantes.Location = new System.Drawing.Point(152, 110);
+            this.lblTripulantes.Name = "lblTripulantes";
+            this.lblTripulantes.Size = new System.Drawing.Size(111, 18);
+            this.lblTripulantes.TabIndex = 2;
+            this.lblTripulantes.Text = "Max Tripulantes:";
+            // 
+            // lblPlanetaOrigem
+            // 
+            this.lblPlanetaOrigem.AutoSize = true;
+            this.lblPlanetaOrigem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanetaOrigem.Location = new System.Drawing.Point(152, 141);
+            this.lblPlanetaOrigem.Name = "lblPlanetaOrigem";
+            this.lblPlanetaOrigem.Size = new System.Drawing.Size(122, 18);
+            this.lblPlanetaOrigem.TabIndex = 3;
+            this.lblPlanetaOrigem.Text = "Planeta de Origem:";
+            // 
+            // nudAbduzidos
+            // 
+            this.nudAbduzidos.Location = new System.Drawing.Point(265, 77);
+            this.nudAbduzidos.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudAbduzidos.Name = "nudAbduzidos";
+            this.nudAbduzidos.Size = new System.Drawing.Size(120, 20);
+            this.nudAbduzidos.TabIndex = 4;
+            // 
+            // nudTripulantes
+            // 
+            this.nudTripulantes.Location = new System.Drawing.Point(270, 107);
+            this.nudTripulantes.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.nudTripulantes.Name = "nudTripulantes";
+            this.nudTripulantes.Size = new System.Drawing.Size(120, 20);
+            this.nudTripulantes.TabIndex = 5;
+            // 
+            // cmbPlaneta
+            // 
+            this.cmbPlaneta.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPlaneta.FormattingEnabled = true;
+            this.cmbPlaneta.Location = new System.Drawing.Point(281, 137);
+            this.cmbPlaneta.Name = "cmbPlaneta";
+            this.cmbPlaneta.Size = new System.Drawing.Size(121, 21);
+            this.cmbPlaneta.TabIndex = 7;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnIniciar.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(155, 173);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(247, 30);
+            this.btnIniciar.TabIndex = 8;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // pbNave
+            // 
+            this.pbNave.Image = global::OVINIManager.Properties.Resources.um_desenho_animado_de_um_alienigena_verde_em_uma_nave_espacial_verde_896071_146;
+            this.pbNave.Location = new System.Drawing.Point(12, 47);
+            this.pbNave.Name = "pbNave";
+            this.pbNave.Size = new System.Drawing.Size(134, 156);
+            this.pbNave.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbNave.TabIndex = 6;
+            this.pbNave.TabStop = false;
+            // 
+            // Inicializador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 218);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.cmbPlaneta);
+            this.Controls.Add(this.pbNave);
+            this.Controls.Add(this.nudTripulantes);
+            this.Controls.Add(this.nudAbduzidos);
+            this.Controls.Add(this.lblPlanetaOrigem);
+            this.Controls.Add(this.lblTripulantes);
+            this.Controls.Add(this.lblAbduzidos);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Inicializador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicializador";
+            this.Load += new System.EventHandler(this.Inicializador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAbduzidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTripulantes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNave)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblAbduzidos;
+        private System.Windows.Forms.Label lblTripulantes;
+        private System.Windows.Forms.Label lblPlanetaOrigem;
+        private System.Windows.Forms.NumericUpDown nudAbduzidos;
+        private System.Windows.Forms.NumericUpDown nudTripulantes;
+        private System.Windows.Forms.PictureBox pbNave;
+        private System.Windows.Forms.ComboBox cmbPlaneta;
+        private System.Windows.Forms.Button btnIniciar;
+    }
+}
---
+++ OVINIManager/Painel.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace OVINIManager
+{
+    partial class Painel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbDados = new System.Windows.Forms.GroupBox();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.lblAbduzidos = new System.Windows.Forms.Label();
+            this.lblTripulantes = new System.Windows.Forms.Label();
+            this.btnLigar = new System.Windows.Forms.Button();
+            this.btnDesligar = new System.Windows.Forms.Button();
+            this.btnAddTripulante = new System.Windows.Forms.Button();
+            this.btnRemoverTripulante = new System.Windows.Forms.Button();
+            this.btnAbduzir = new System.Windows.Forms.Button();
+            this.btnDesabduzir = new System.Windows.Forms.Button();
+            this.btnOrigem = new System.Windows.Forms.Button();
+            this.lblPlaneta = new System.Windows.Forms.Label();
+            this.cmbPlanetas = new System.Windows.Forms.ComboBox();
+            this.btnMudarPlaneta = new System.Windows.Forms.Button();
+            this.pbRaios = new System.Windows.Forms.PictureBox();
+            this.grbDados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbDados
+            // 
+            this.grbDados.Controls.Add(this.lblPlaneta);
+            this.grbDados.Controls.Add(this.lblTripulantes);
+            this.grbDados.Controls.Add(this.lblAbduzidos);
+            this.grbDados.Controls.Add(this.lblSituacao);
+            this.grbDados.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbDados.Location = new System.Drawing.Point(35, 14);
+            this.grbDados.Name = "grbDados";
+            this.grbDados.Size = new System.Drawing.Size(213, 185);
+            this.grbDados.TabIndex = 0;
+            this.grbDados.TabStop = false;
+            this.grbDados.Text = "Dados";
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSituacao.Location = new System.Drawing.Point(7, 30);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(65, 18);
+            this.lblSituacao.TabIndex = 0;
+            this.lblSituacao.Text = "Situação:";
+            // 
+            // lblAbduzidos
+            // 
+            this.lblAbduzidos.AutoSize = true;
+            this.lblAbduzidos.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbduzidos.Location = new System.Drawing.Point(7, 70);
+            this.lblAbduzidos.Name = "lblAbduzidos";
+            this.lblAbduzidos.Size = new System.Drawing.Size(76, 18);
+            this.lblAbduzidos.TabIndex = 1;
+            this.lblAbduzidos.Text = "Abduzidos:";
+            // 
+            // lblTripulantes
+            // 
+            this.lblTripulantes.AutoSize = true;
+            this.lblTripulantes.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTripulantes.Location = new System.Drawing.Point(7, 113);
+            this.lblTripulantes.Name = "lblTripulantes";
+            this.lblTripulantes.Size = new System.Drawing.Size(81, 18);
+            this.lblTripulantes.TabIndex = 2;
+            this.lblTripulantes.Text = "Tripulantes:";
+            // 
+            // btnLigar
+            // 
+            this.btnLigar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLigar.Location = new System.Drawing.Point(35, 205);
+            this.btnLigar.Name = "btnLigar";
+            this.btnLigar.Size = new System.Drawing.Size(213, 23);
+            this.btnLigar.TabIndex = 1;
+            this.btnLigar.Text = "LIGAR";
+            this.btnLigar.UseVisualStyleBackColor = true;
+            this.btnLigar.Click += new System.EventHandler(this.btnLigar_Click);
+            // 
+            // btnDesligar
+            // 
+            this.btnDesligar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesligar.Location = new System.Drawing.Point(35, 234);
+            this.btnDesligar.Name = "btnDesligar";
+            this.btnDesligar.Size = new System.Drawing.Size(213, 23);
+            this.btnDesligar.TabIndex = 2;
+            this.btnDesligar.Text = "DESLIGAR";
+            this.btnDesligar.UseVisualStyleBackColor = true;
+            this.btnDesligar.Click += new System.EventHandler(this.btnDesligar_Click);
+            // 
+            // btnAddTripulante
+            // 
+            this.btnAddTripulante.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTripulante.Location = new System.Drawing.Point(254, 24);
+            this.btnAddTripulante.Name = "btnAddTripulante";
+            this.btnAddTripulante.Size = new System.Drawing.Size(145, 23);
+            this.btnAddTripulante.TabIndex = 3;
+            this.btnAddTripulante.Text = "Adicionar Tripulante";
+            this.btnAddTripulante.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoverTripulante
+            // 
+            this.btnRemoverTripulante.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoverTripulante.Location = new System.Drawing.Point(254, 53);
+            this.btnRemoverTripulante.Name = "btnRemoverTripulante";
+            this.btnRemoverTripulante.Size = new System.Drawing.Size(145, 23);
+            this.btnRemoverTripulante.TabIndex = 4;
+            this.btnRemoverTripulante.Text = "Remover Tripulante";
+            this.btnRemoverTripulante.UseVisualStyleBackColor = true;
+            // 
+            // btnAbduzir
+            // 
+            this.btnAbduzir.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbduzir.Location = new System.Drawing.Point(254, 82);
+            this.btnAbduzir.Name = "btnAbduzir";
+            this.btnAbduzir.Size = new System.Drawing.Size(145, 23);
+            this.btnAbduzir.TabIndex = 5;
+            this.btnAbduzir.Text = "Abduzir";
+            this.btnAbduzir.UseVisualStyleBackColor = true;
+            // 
+            // btnDesabduzir
+            // 
+            this.btnDesabduzir.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesabduzir.Location = new System.Drawing.Point(254, 111);
+            this.btnDesabduzir.Name = "btnDesabduzir";
+            this.btnDesabduzir.Size = new System.Drawing.Size(145, 23);
+            this.btnDesabduzir.TabIndex = 6;
+            this.btnDesabduzir.Text = "Desabduzir";
+            this.btnDesabduzir.UseVisualStyleBackColor = true;
+            // 
+            // btnOrigem
+            // 
+            this.btnOrigem.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrigem.Location = new System.Drawing.Point(254, 140);
+            this.btnOrigem.Name = "btnOrigem";
+            this.btnOrigem.Size = new System.Drawing.Size(145, 25);
+            this.btnOrigem.TabIndex = 7;
+            this.btnOrigem.Text = "Retornar a Origem";
+            this.btnOrigem.UseVisualStyleBackColor = true;
+            // 
+            // lblPlaneta
+            // 
+            this.lblPlaneta.AutoSize = true;
+            this.lblPlaneta.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlaneta.Location = new System.Drawing.Point(7, 154);
+            this.lblPlaneta.Name = "lblPlaneta";
+            this.lblPlaneta.Size = new System.Drawing.Size(56, 18);
+            this.lblPlaneta.TabIndex = 3;
+            this.lblPlaneta.Text = "Planeta:";
+            // 
+            // cmbPlanetas
+            // 
+            this.cmbPlanetas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPlanetas.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPlanetas.FormattingEnabled = true;
+            this.cmbPlanetas.Location = new System.Drawing.Point(254, 171);
+            this.cmbPlanetas.Name = "cmbPlanetas";
+            this.cmbPlanetas.Size = new System.Drawing.Size(74, 23);
+            this.cmbPlanetas.TabIndex = 8;
+            // 
+            // btnMudarPlaneta
+            // 
+            this.btnMudarPlaneta.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMudarPlaneta.Location = new System.Drawing.Point(335, 171);
+            this.btnMudarPlaneta.Name = "btnMudarPlaneta";
+            this.btnMudarPlaneta.Size = new System.Drawing.Size(64, 23);
+            this.btnMudarPlaneta.TabIndex = 9;
+            this.btnMudarPlaneta.Text = "Mudar";
+            this.btnMudarPlaneta.UseVisualStyleBackColor = true;
+            // 
+            // pbRaios
+            // 
+            this.pbRaios.Image = global::OVINIManager.Properties.Resources._9dce74fd_capa_blog_raios_trovoes_relampagos;
+            this.pbRaios.Location = new System.Drawing.Point(255, 205);
+            this.pbRaios.Name = "pbRaios";
+            this.pbRaios.Size = new System.Drawing.Size(144, 50);
+            this.pbRaios.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbRaios.TabIndex = 10;
+            this.pbRaios.TabStop = false;
+            // 
+            // Painel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 275);
+            this.Controls.Add(this.pbRaios);
+            this.Controls.Add(this.btnMudarPlaneta);
+            this.Controls.Add(this.cmbPlanetas);
+            this.Controls.Add(this.btnOrigem);
+            this.Controls.Add(this.btnDesabduzir);
+            this.Controls.Add(this.btnAbduzir);
+            this.Controls.Add(this.btnRemoverTripulante);
+            this.Controls.Add(this.btnAddTripulante);
+            this.Controls.Add(this.btnDesligar);
+            this.Controls.Add(this.btnLigar);
+            this.Controls.Add(this.grbDados);
+            this.Name = "Painel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Painel";
+            this.Load += new System.EventHandler(this.Painel_Load);
+            this.grbDados.ResumeLayout(false);
+            this.grbDados.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbDados;
+        private System.Windows.Forms.Label lblTripulantes;
+        private System.Windows.Forms.Label lblAbduzidos;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.Button btnLigar;
+        private System.Windows.Forms.Button btnDesligar;
+        private System.Windows.Forms.Button btnAddTripulante;
+        private System.Windows.Forms.Button btnRemoverTripulante;
+        private System.Windows.Forms.Button btnAbduzir;
+        private System.Windows.Forms.Button btnDesabduzir;
+        private System.Windows.Forms.Button btnOrigem;
+        private System.Windows.Forms.Label lblPlaneta;
+        private System.Windows.Forms.ComboBox cmbPlanetas;
+        private System.Windows.Forms.Button btnMudarPlaneta;
+        private System.Windows.Forms.PictureBox pbRaios;
+    }
+}
